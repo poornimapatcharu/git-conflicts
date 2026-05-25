914aa41f2ba1b38caba842bbefdfc2647e08d2f3
poornimapatcharu <poornimapatcharu@gmail.com>
2026-05-25 23:05:43 +1000

    Update app.py

--- app.py
+++ app.py
@@ -9,6 +9,6 @@ def home():
 
 if __name__ == "__main__":
     # Get port from OS environment variable
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 1000))
 
     app.run(host="0.0.0.0", port=port)
